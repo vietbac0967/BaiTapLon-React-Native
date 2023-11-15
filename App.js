fd0d0b0ee1b6d2d72961ef445ea4d1dee57ebe7a
VoQuocHuy <voquochuy042@gmail.com>
2023-11-15 17:05:19 +0700

    15/11/2023 Update SettingScreen

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from "./StackNavigator";
 import SignupScreen from './screens/SignupScreen';
+import SettingScreen from './screens/SettingScreen';
 export default function App() {
   return (
-    <SignupScreen />
+    // <SignupScreen />
+    <SettingScreen />
   );
 }
 
